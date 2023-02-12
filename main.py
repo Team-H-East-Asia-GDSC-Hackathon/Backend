d687a12652bb739387cc84d2bcc8b700cce176c0
Ryusuke Haruta <flyrb7@gmail.com>
2023-02-12 09:19:09 +0900

    modify Procfile

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import json
-
-from fastapi import FastAPI, File, UploadFile, APIRouter
-from fastapi.responses import FileResponse,Response
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI,  HTTPException
 
 app = FastAPI()
-# router = APIRouter()
-
-# origins = [
-#     "http://localhost:3000",
-# ]
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 @app.get("/")
 async def Hello():
     return {"Hello":"Vegan!"}
 
-@app.get(path="/diet/info/")
+@app.get(path="/diet/info/", status_code=200)
 async def get_info(date:str,time:str):
     f = open('menu.json', 'r')
     json_dict = json.load(f)
     for i in range(len(json_dict)):
         if str(date) == json_dict[i]['date']  and str(time) == json_dict[i]['time']:
             return json.dumps(json_dict[i])
+        else:
+            raise HTTPException(status_code=400, detail="Model not found.")
